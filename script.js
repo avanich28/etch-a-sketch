2daa847db6105ecf81a37de1ae22bf96ec8dd6d2
avanich28 <arpawan.v@gmail.com>
2023-04-24 17:56:44 +0700

    add click sound, erase mode, and draw type while mousepress)

--- script.js
+++ script.js
@@ -4,20 +4,33 @@ const container = document.querySelector('.container');
 const fillSize = document.querySelector('.size');
 const submitBtn = document.querySelector('.submit');
 const resetBtn = document.querySelector('.reset');
-const modes = document.querySelector('#color-mode');
-const color = document.querySelector('#colorPicker');
-const sound = document.querySelector('audio.typewriter');
+const modes = document.getElementById('color-mode');
+const color = document.getElementById('colorPicker');
+const drawChoice = document.querySelectorAll('input[name="choice"]');
+const typewriterAudio = document.getElementById('typewriter');
+const clickAudio = document.getElementById('click');
 
-// Store user latest input
-// Default value
+// Store user latest input (Default value)
 let latestUserSizeNum = 16;
 let userSelectMode = 'single';
+let userDrawType = 'hover';
 
 // Define box value in order to add box color
 let allBox;
 
-// Color Mode
-const selectMode = function (mode) {
+// mouse action
+let mousePress = false;
+document.body.addEventListener('mousedown', e => {
+  e.stopPropagation(); // stop input[type=number] continuously increase
+  mousePress = true;
+});
+document.body.addEventListener('mouseup', e => {
+  e.stopPropagation();
+  mousePress = false;
+});
+
+// Color Mode & Draw Type
+const selectMode = function (mode, drawType) {
   // For rainbow mode
   const randomRgb = function () {
     const numR = Math.trunc(Math.random() * 255);
@@ -26,26 +39,51 @@ const selectMode = function (mode) {
     return `rgb(${numR}, ${numG}, ${numB})`;
   };
 
-  // Set mode condition
-  if (mode === 'single') {
-    allBox.forEach(el => {
-      el.addEventListener(
-        'mouseover',
-        () => (el.style.backgroundColor = `${color.value}`)
-      );
+  // Color mode
+  const modeColor = function (mode, e) {
+    if (mode === 'single') {
+      e.target.style.backgroundColor = `${color.value}`;
+    } else if (mode === 'rainbow') {
+      e.target.style.backgroundColor = randomRgb();
+    } else if (mode === 'erase') {
+      e.target.style.backgroundColor = 'rgb(233, 239, 243)'
+    }
+  };
+
+  // Clear previous Event Listener
+  allBox.forEach(el => {
+    el.removeEventListener('mouseover', e => {
+      if (!mousePress || mousePress) {
+        modeColor(mode, e);
+      }
     });
-  } else if (mode === 'rainbow') {
+  });
+
+  // Draw Type
+  if (drawType === 'hover') {
     allBox.forEach(el => {
-      el.addEventListener(
-        'mouseover',
-        () => (el.style.backgroundColor = randomRgb())
-      );
+      el.addEventListener('mouseover', e => {
+        if (!mousePress) {
+          modeColor(mode, e);
+        }
+      });
+    });
+  } else if (drawType === 'press') {
+    allBox.forEach(el => {
+      el.addEventListener('mouseover', e => {
+        if (mousePress) {
+          modeColor(mode, e);
+        }
+      });
     });
   }
 };
-
 // Set grid dimension
-const createGrid = function (size = latestUserSizeNum, mode = userSelectMode) {
+const createGrid = function (
+  size = latestUserSizeNum,
+  mode = userSelectMode,
+  drawType = userDrawType
+) {
   // Clear container
   container.innerHTML = '';
 
@@ -71,15 +109,16 @@ const createGrid = function (size = latestUserSizeNum, mode = userSelectMode) {
   allBox = document.querySelectorAll('.box');
 
   // Set mode condition
-  selectMode(mode);
+  selectMode(mode, drawType);
 };
+
 createGrid(16); // First play
 
 // Return grid Size value
 const calcSize = function (e, num = e.target.value) {
   e.preventDefault();
   latestUserSizeNum = num;
-  return createGrid(num);
+  return createGrid(num, userSelectMode, userDrawType);
 };
 
 // Add event Listener on input box
@@ -89,12 +128,14 @@ fillSize.addEventListener('change', calcSize);
 const submitInput = function (e) {
   if (e) {
     e.preventDefault();
+
     if (fillSize.value === '') {
       calcSize(e, 16);
     } else {
       calcSize(e, fillSize.value);
     }
     fillSize.value = '';
+    typewriterAudio.play();
   }
 };
 submitBtn.addEventListener('click', submitInput);
@@ -104,36 +145,25 @@ const resetGrid = function (e) {
   e.preventDefault();
   createGrid();
   fillSize.value = '';
+  typewriterAudio.play();
 };
 resetBtn.addEventListener('click', resetGrid);
 
-// Add event listener on modes
+// Add event listener on color modes
 modes.addEventListener('change', e => {
-  userSelectMode = e.target.value;
-  selectMode(userSelectMode);
+  const modeColor = e.target.value;
+  userSelectMode = modeColor;
+  selectMode(modeColor, userDrawType);
+  clickAudio.play();
 });
 
-sound.play();
-sound.play();
-sound.play();
-sound.play();
-// Add sound on animation
-let i = 0;
-if ((i = 0)) {
-  setTimeout(function () {
-    sound.currentTime = 0;
-    sound.play();
-  }, 5000);
-} else if ((i = 11)) {
-  audio.pause();
-} else {
-  sound.currentTime = 0;
-  sound.play();
-}
-for (let i = 0; i < 11; i++) {
-  if ((i = 0)) {
-    setTimeout(() => sound.play(), 5000);
-  } else {
-    sound.play();
-  }
-}
+// Add event listener on draw types
+drawChoice.forEach(el => {
+  el.addEventListener('change', e => {
+    const modeDraw = e.target.value;
+    userDrawType = modeDraw;
+    // console.log(userDrawType);
+    createGrid(latestUserSizeNum, userSelectMode, modeDraw);
+    clickAudio.play();
+  });
+});
